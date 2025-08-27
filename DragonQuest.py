2539500110cd9f39888c241c2f801aa56c331d06
icymilkytea <megarobot64@gmail.com>
2025-08-27 18:17:05 +0300

    мини текстовый квест

---
+++ DragonQuest.py
@@ -0,0 +1,30 @@
+invertory = []
+
+def start_screen():
+    while True:
+        user_input = (input("Ты стоишь темной зловещей пещерой? Войти в нее? ")).lower()
+        if user_input == "да":
+            cave()
+            return
+        elif user_input == "нет":
+            print("Ты недостаточно храбр. Попробуй другое приключение.")
+            return
+        elif user_input == "взять факел":
+            if "факел" not in invertory:
+                print("Зная что в пещере темно ты взял факел.")
+                invertory.append("факел")
+            else:
+                print("У тебя уже есть факел.")
+            continue
+        else:
+            print("Твой выбор непонятен.")
+            continue
+
+def cave():
+        if "факел" not in invertory:
+            print("Ты внутри пещеры и забыл взять факел. Даже вход не видно и довольно скоро ты погибаешь")
+            print("В следующий раз попробуй взять факел")
+        else:
+            print("Ты зажег факел и смог выбаться. Победа.")
+
+start_screen()
